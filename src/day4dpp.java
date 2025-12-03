0831a0f86c159a43d572f5aa89b67542ac52a676
Dikshas09 <dikshasinghal858@gmail.com>
2025-12-03 18:39:22 +0530

    dpp

--- src/day4dpp.java
+++ src/day4dpp.java
@@ -2,14 +2,14 @@ import java.util.*;
 public class day4dpp {
     public static void main(String[] arg) {
         Scanner sc= new Scanner(System.in);
-    // question 1......................
+// question 1..................................................................
         for(int i=1;i<=4;i++){
             for(int j=1;j<=4;j++){
                 System.out.print(i);
             }
             System.out.println("");
         }
-     // question 2  ......................
+// question 2  ...................................................................
         int n1= sc.nextInt();
         for(int i=1;i<=n1;i++){
             for(int j=1;j<=n1+1-i;j++){
@@ -17,7 +17,7 @@ public class day4dpp {
             }
             System.out.println("");
         }
-    // question 3......................
+// question 3.................................................................
         int n2= sc.nextInt();
         for(int i=1;i<=n2;i++) {
             for (int j = 1; j <= i; j++) {
@@ -25,7 +25,7 @@ public class day4dpp {
             }
             System.out.println("");
         }
-     // question 4......................
+// question 4.................................................................
         int n3= sc.nextInt();
         for(int i=1;i<=n3+1;i++) {
             for (int j = 1; j <= i; j++) {
@@ -38,7 +38,7 @@ public class day4dpp {
             }
             System.out.println("");
         }
-     // question 5......................
+ // question 5............................................................
         int n4= sc.nextInt();
         for(int i=1;i<=n4;i++) {
             for (int j = 1; j <= i; j++) {
@@ -52,7 +52,7 @@ public class day4dpp {
             }
             System.out.println("");
         }
-     // question 6......................
+// question 6.......................................................
         int n5= sc.nextInt();
         int m= sc.nextInt();
         for(int i=1;i<=n5;i++) {
@@ -69,7 +69,7 @@ public class day4dpp {
             }
             System.out.println("");
         }
-     // question 7......................
+// question 7..........................................................
         int n6= sc.nextInt();
         for(int i=1;i<=n6;i++) {
             for (int j = 1; j <= i; j++) {
@@ -77,7 +77,7 @@ public class day4dpp {
             }
             System.out.println("");
       }
-     // question 8......................
+// question 8.....................................................................
         int n7= sc.nextInt();
         for(int i=1;i<=n7;i++) {
             for (int j = 1; j <= i; j++) {
@@ -85,7 +85,7 @@ public class day4dpp {
             }
             System.out.println("");
         }
-    // question 9......................
+// question 9...................................................
         int n8= sc.nextInt();
         for(int i=1;i<=n8;i++) {
             for (int j = i; j >= 1; j--) {
@@ -93,7 +93,7 @@ public class day4dpp {
             }
             System.out.println("");
         }
-     // question 10............................................
+// question 10............................................
         int n9= sc.nextInt();
         for(int i=1;i<=n9;i++) {
             for (int j = 1; j <= i; j++) {
